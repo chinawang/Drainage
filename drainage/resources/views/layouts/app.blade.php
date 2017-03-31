fa61b3f51566f15599fe947fb2d37e41bb33b3fa
Wangyx <chinawangyx@hotmail.com>
2017-03-31 20:03:48 +0800

    update some css

--- drainage/resources/views/layouts/app.blade.php
+++ drainage/resources/views/layouts/app.blade.php
@@ -86,5 +86,6 @@
     <!-- Scripts -->
 {{--    <script src="{{ asset('js/app.js') }}"></script>--}}
     <script src="http://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="http://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
 </body>
 </html>
