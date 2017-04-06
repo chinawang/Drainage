2d1bae1ee87c12243c734d45d39c5e7e430c05bb
Wangyx <chinawangyx@hotmail.com>
2017-04-07 00:24:42 +0800

    add somme controller Code

--- drainage/app/Http/Validations/Station/StationValidation.php
+++ drainage/app/Http/Validations/Station/StationValidation.php
@@ -9,9 +9,77 @@
 namespace App\Http\Validations\Station;
 
 
+use App\Http\Logic\Station\StationLogic;
 use App\Http\Validations\Validation;
 
 class StationValidation extends Validation
 {
+    protected $stationLogic;
 
+    public function __construct(Request $request,StationLogic $stationLogic)
+    {
+        parent::__construct($request);
+        $this->stationLogic = $stationLogic;
+    }
+
+    public function storeNewStation()
+    {
+        $input = $this->filterRequest([
+            'station_number','name','address','lat','lng'
+        ]);
+
+        $rules = [
+            'station_number' => ['string'],
+            'name' => ['string'],
+            'address' => ['string'],
+            'lat' => ['string'],
+            'lng' => ['string'],
+        ];
+
+        $validator = Validator::make($input,$rules);
+        if ($validator->fails()) {
+            throw new BadRequestException($validator->errors());
+        }
+
+        return $input;
+    }
+
+    public function updateStation($stationID)
+    {
+        $input = $this->filterRequest([
+            'station_number','name','address','lat','lng'
+        ]);
+
+        $rules = [
+            'station_number' => ['string'],
+            'name' => ['string'],
+            'address' => ['string'],
+            'lat' => ['string'],
+            'lng' => ['string'],
+        ];
+
+        $validator = Validator::make($input,$rules);
+        if ($validator->fails()) {
+            throw new BadRequestException($validator->errors());
+        }
+
+        $station= $this->stationLogic->findStation($stationID);
+        if($station->id != $stationID) {
+            throw new ForbiddenException();
+        }
+
+        return $input;
+    }
+
+    public function deleteStation()
+    {
+        $input = $this->filterRequest(['id']);
+        $stationID = json_decode($input['id']);
+
+        if (empty($stationID)) {
+            throw new BadRequestException();
+        }
+
+        return $stationID;
+    }
 }
--- drainage/app/Http/Validations/User/UserValidation.php
+++ drainage/app/Http/Validations/User/UserValidation.php
@@ -41,7 +41,7 @@ class UserValidation extends Validation
         ]);
 
         $rules = [
-            'name'  => ['required', 'string'],
+            'name'  => ['required', 'string','unique:users'],
             'employee_number' => ['string'],
             'realname' => ['string'],
             'office' => ['string'],
@@ -57,6 +57,79 @@ class UserValidation extends Validation
         return $input;
     }
 
+    /**
+     * @param $userID
+     * @return array
+     */
+    public function updateUser($userID)
+    {
+        $input = $this->filterRequest([
+            'employee_number','realname','office','contact','password'
+        ]);
 
+        $rules = [
+            'employee_number' => ['string'],
+            'realname' => ['string'],
+            'office' => ['string'],
+            'contact' => ['string'],
+            'password' => ['required', 'string', 'min:6'],
+        ];
+
+        $validator = Validator::make($input,$rules);
+        if ($validator->fails()) {
+            throw new BadRequestException($validator->errors());
+        }
+
+        $user = $this->userLogic->findUser($userID);
+        if($user->id != $userID) {
+            throw new ForbiddenException();
+        }
+
+        return $input;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function deleteUser()
+    {
+        $input = $this->filterRequest(['id']);
+        $userID = json_decode($input['id']);
+
+        if (empty($userID)) {
+            throw new BadRequestException();
+        }
+
+        return $userID;
+    }
+
+    /**
+     * @param $userID
+     * @return array
+     */
+    public function resetPassword($userID)
+    {
+        $input = $this->filterRequest(['old', 'new']);
+
+        $rules = [
+            'old' => ['required', 'string'],
+            'new' => ['required', 'string', 'min:6'],
+        ];
+
+        $validator = Validator::make($input, $rules);
+
+        if ($validator->fails()) {
+            throw new BadRequestException($validator->errors());
+        }
+
+        $user = $this->userLogic->findUser($userID);
+
+        if (! Hash::check($input['old'], $user->password)) {
+            throw new ForbiddenException(trans('old_password_error'));
+        }
+
+
+        return $input;
+    }
 
 }
