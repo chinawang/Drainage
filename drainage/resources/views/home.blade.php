872cb8b505228a9db47aba68446caf08f7a7a3a5
Wangyx <chinawangyx@hotmail.com>
2017-04-08 17:40:24 +0800

    add header css code

--- drainage/resources/views/home.blade.php
+++ drainage/resources/views/home.blade.php
@@ -8,73 +8,73 @@
                 <div class="panel-body">
                     <ul class="main-menu">
                         <li class="station">
-                            <a href="/station/lists">
+                            <a href="/station/lists" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>泵站资料管理</h4>
                             </a>
                         </li>
                         <li class="equipment">
-                            <a href="/equipment/lists">
+                            <a href="/equipment/lists" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>设备资料管理</h4>
                             </a>
                         </li>
                         <li class="failure">
-                            <a href="/failure/lists">
+                            <a href="/failure/lists" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>设备维修管理</h4>
                             </a>
                         </li>
                         <li class="map">
-                            <a href="">
+                            <a href="" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>泵站地图</h4>
                             </a>
                         </li>
                         <li class="status">
-                            <a href="">
+                            <a href="" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>泵站工作状态</h4>
                             </a>
                         </li>
                         <li class="station-warning">
-                            <a href="">
+                            <a href="" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>泵站报警管理</h4>
                             </a>
                         </li>
                         <li class="report">
-                            <a href="">
+                            <a href="" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>统计报表</h4>
                             </a>
                         </li>
                         <li class="weather">
-                            <a href="">
+                            <a href="" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>天气预报</h4>
                             </a>
                         </li>
                         <li class="user">
-                            <a href="/user/lists">
+                            <a href="/user/lists" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>账户管理</h4>
                             </a>
                         </li>
                         <li class="role">
-                            <a href="/role/lists">
+                            <a href="/role/lists" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>角色管理</h4>
                             </a>
                         </li>
                         <li href="permission">
-                            <a href="/permission/lists">
+                            <a href="/permission/lists" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>行为权限管理</h4>
                             </a>
                         </li>
                         <li class="log">
-                            <a href="">
+                            <a href="" class="menu-item">
                                 <img class="menu-icon" src="/img/home/test.png">
                                 <h4>系统日志管理</h4>
                             </a>
