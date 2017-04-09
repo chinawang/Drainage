ce4903d04bcc63c278153eaccf19d834193d4d81
Wangyx <chinawangyx@hotmail.com>
2017-04-09 12:55:13 +0800

    update home.css

--- drainage/resources/views/station/list.blade.php
+++ drainage/resources/views/station/list.blade.php
@@ -7,10 +7,27 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12 col-md-offset-0">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
-
+                    <div class="panel-heading">泵站列表</div>
+                    <table class="table table-striped table-hover ">
+                        <thead>
+                        <tr>
+                            <th>编号</th>
+                            <th>泵站名称</th>
+                            <th>泵站地址</th>
+                            <th>操作</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                        </tbody>
+                    </table>
                     <div class="panel-body">
                         Station
                     </div>
