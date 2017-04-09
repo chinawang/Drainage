7cd98f21b2e3142e2489026f932fc2881fee0890
Wangyx <chinawangyx@hotmail.com>
2017-04-09 14:49:20 +0800

    update home.css

--- drainage/resources/views/station/list.blade.php
+++ drainage/resources/views/station/list.blade.php
@@ -12,31 +12,30 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 col-md-offset-0">
-                <div class="panel panel-default">
+                <div class="panel panel-default custom-panel">
                     <div class="panel-heading">
                         泵站列表
-                        <a href="#" class="btn btn-primary add-station">添加泵站</a>
+                        <a href="#" class="btn btn-primary btn-sm">添加泵站</a>
                     </div>
-                    <table class="table table-striped table-hover ">
-                        <thead>
-                        <tr>
-                            <th>编号</th>
-                            <th>泵站名称</th>
-                            <th>泵站地址</th>
-                            <th>操作</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Column content</td>
-                            <td>Column content</td>
-                            <td>Column content</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                    <div class="panel-body">
-                        Station
+                    <div class="panel-body custom-panel-body">
+                        <table class="table table-striped table-hover ">
+                            <thead>
+                            <tr>
+                                <th>编号</th>
+                                <th>泵站名称</th>
+                                <th>泵站地址</th>
+                                <th>操作</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
