55771950672d23e7f0d719c5987ec113aedbcb5c
Wangyx <chinawangyx@hotmail.com>
2017-04-10 15:45:57 +0800

    update html code

---
+++ drainage/resources/views/failure/updateFailure.blade.php
@@ -0,0 +1,154 @@
+@extends('layouts.app')
+
+@section('stylesheet')
+    <link href="{{ asset('css/failure/failure.css') }}" rel="stylesheet">
+@endsection
+
+@section('subtitle')
+    <span>设备故障管理</span>
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default custom-panel">
+                    <div class="panel-heading">
+                        编辑故障记录
+                        <a href="javascript:history.back(-1)" class="btn-link">返回</a>
+                    </div>
+                    <div class="panel-body custom-panel-body">
+                        <form class="form-horizontal" role="form" method="POST" action="#">
+
+                            <div class="form-group{{ $errors->has('station') ? ' has-error' : '' }}">
+                                <label for="station" class="col-md-4 control-label">所属泵站</label>
+
+                                <div class="col-md-6">
+                                    <select class="form-control" id="select">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                    @if ($errors->has('station'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('station') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('equipment') ? ' has-error' : '' }}">
+                                <label for="equipment" class="col-md-4 control-label">故障设备</label>
+
+                                <div class="col-md-6">
+                                    <select class="form-control" id="select">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                    @if ($errors->has('equipment'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('equipment') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('failure_type') ? ' has-error' : '' }}">
+                                <label for="failure_type" class="col-md-4 control-label">故障类型</label>
+
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="failure_type" value="{{ old('failure_type') }}" placeholder="请输入故障类型" required >
+
+                                    @if ($errors->has('failure_type'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('failure_type') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('failure_description') ? ' has-error' : '' }}">
+                                <label for="failure_description" class="col-md-4 control-label">故障描述</label>
+
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="failure_description" value="{{ old('failure_description') }}" placeholder="请输入故障描述" required >
+
+                                    @if ($errors->has('failure_description'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('failure_description') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('	equipment_status') ? ' has-error' : '' }}">
+                                <label for="equipment" class="col-md-4 control-label">设备状态</label>
+
+                                <div class="col-md-6">
+                                    <select class="form-control" id="select">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                    @if ($errors->has('	equipment_status'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('	equipment_status') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('reporter') ? ' has-error' : '' }}">
+                                <label for="reporter" class="col-md-4 control-label">报修人</label>
+
+                                <div class="col-md-6">
+                                    <select class="form-control" id="select">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                    @if ($errors->has('reporter'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('reporter') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('report_at') ? ' has-error' : '' }}">
+                                <label for="report_at" class="col-md-4 control-label">报修时间</label>
+
+                                <div class="col-md-6">
+                                    <input type="datetime" class="form-control" name="report_at" value="{{ old('report_at') }}" placeholder="请输入报修时间" required >
+
+                                    @if ($errors->has('report_at'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('report_at') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary btn-custom">
+                                        保存
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ drainage/resources/views/maintenance/updateMaintenance.blade.php
@@ -0,0 +1,135 @@
+@extends('layouts.app')
+
+@section('stylesheet')
+    <link href="{{ asset('css/maintenance/maintenance.css') }}" rel="stylesheet">
+@endsection
+
+@section('subtitle')
+    <span>设备维修管理</span>
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default custom-panel">
+                    <div class="panel-heading">
+                        编辑维修记录
+                        <a href="javascript:history.back(-1)" class="btn-link">返回</a>
+                    </div>
+                    <div class="panel-body custom-panel-body">
+                        <form class="form-horizontal" role="form" method="POST" action="#">
+
+                            <div class="form-group{{ $errors->has('station') ? ' has-error' : '' }}">
+                                <label for="station" class="col-md-4 control-label">所属泵站</label>
+
+                                <div class="col-md-6">
+                                    <select class="form-control" id="select">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                    @if ($errors->has('station'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('station') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('equipment') ? ' has-error' : '' }}">
+                                <label for="equipment" class="col-md-4 control-label">故障设备</label>
+
+                                <div class="col-md-6">
+                                    <select class="form-control" id="select">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                    @if ($errors->has('equipment'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('equipment') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('failure_reason') ? ' has-error' : '' }}">
+                                <label for="failure_reason" class="col-md-4 control-label">故障原因</label>
+
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="failure_reason" value="{{ old('failure_reason') }}" placeholder="请输入故障原因" required >
+
+                                    @if ($errors->has('failure_reason'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('failure_reason') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('repair_solution') ? ' has-error' : '' }}">
+                                <label for="repair_solution" class="col-md-4 control-label">解决办法</label>
+
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="repair_solution" value="{{ old('repair_solution') }}" placeholder="请输入解决办法" required >
+
+                                    @if ($errors->has('repair_solution'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('repair_solution') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('repairer') ? ' has-error' : '' }}">
+                                <label for="repairer" class="col-md-4 control-label">维修人</label>
+
+                                <div class="col-md-6">
+                                    <select class="form-control" id="select">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                    @if ($errors->has('repairer'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('repairer') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('repair_at') ? ' has-error' : '' }}">
+                                <label for="repair_at" class="col-md-4 control-label">维修时间</label>
+
+                                <div class="col-md-6">
+                                    <input type="datetime" class="form-control" name="repair_at" value="{{ old('repair_at') }}" placeholder="请输入维修时间" required >
+
+                                    @if ($errors->has('repair_at'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('repair_at') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary btn-custom">
+                                        保存
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
