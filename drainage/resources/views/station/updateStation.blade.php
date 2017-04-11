1441bca72b12aa6fa530a226fe76662af481b48f
Wangyx <chinawangyx@hotmail.com>
2017-04-12 02:09:24 +0800

    update html code

--- drainage/resources/views/station/updateStation.blade.php
+++ drainage/resources/views/station/updateStation.blade.php
@@ -64,7 +64,7 @@
 
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary btn-custom">
+                                    <button type="submit" class="btn btn-primary btn-custom" data-loading-text="保存中...">
                                         保存
                                     </button>
                                 </div>
