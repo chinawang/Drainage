d4d3ebaf8c5cd7e38ecb361cb3e0f56ea78ee165
Wangyx <chinawangyx@hotmail.com>
2017-04-12 22:26:53 +0800

    update html code

--- drainage/app/Http/Controllers/Station/EquipmentController.php
+++ drainage/app/Http/Controllers/Station/EquipmentController.php
@@ -142,8 +142,6 @@ class EquipmentController extends Controller
             $leader = $this->userInfo($equipment['leader_id']);
             $custodian = $this->userInfo($equipment['custodian_id']);
 
-            return $station;
-
             $equipment['station_name'] = $station['name'];
             $equipment['leader_name'] = $leader['real_name'];
             $equipment['custodian_name'] = $custodian['real_name'];
@@ -151,7 +149,7 @@ class EquipmentController extends Controller
 
         $param = ['equipments' => $equipmentPaginate];
 //        return $param;
-//        return view('equipment.list',$param);
+        return view('equipment.list',$param);
     }
 
     /**
