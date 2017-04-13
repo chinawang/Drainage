d58b41bf24fd2838755eba90f7f95a10a48a54fa
Wangyx <chinawangyx@hotmail.com>
2017-04-14 00:32:09 +0800

    update html code

--- drainage/resources/views/weather/weather.blade.php
+++ drainage/resources/views/weather/weather.blade.php
@@ -55,7 +55,7 @@
             "location": "WW0V9QP93VS8",
             "geolocation": "disabled",
             "position": "top-left",
-            "margin": "20px 20px",
+            "margin": "10px 10px",
             "language": "zh-chs",
             "unit": "c",
             "theme": "chameleon",
