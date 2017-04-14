e55c0ca9f27f96e1ae6afe5174fd95208148659f
Wangyx <chinawangyx@hotmail.com>
2017-04-14 14:30:32 +0800

    update html code

--- drainage/resources/views/weather/weather.blade.php
+++ drainage/resources/views/weather/weather.blade.php
@@ -29,43 +29,6 @@
 
 @section('javascript')
 
-    <script>
-        (function (T, h, i, n, k, P, a, g, e) {
-            g = function () {
-                P = h.createElement(i);
-                a = h.getElementsByTagName(i)[0];
-                P.src = k;
-                P.charset = "utf-8";
-                P.async = 1;
-                a.parentNode.insertBefore(P, a)
-            };
-            T["ThinkPageWeatherWidgetObject"] = n;
-            T[n] || (T[n] = function () {
-                (T[n].q = T[n].q || []).push(arguments)
-            });
-            T[n].l = +new Date();
-            if (T.attachEvent) {
-                T.attachEvent("onload", g)
-            } else {
-                T.addEventListener("load", g, false)
-            }
-        }(window, document, "script", "tpwidget", "//widget.seniverse.com/widget/chameleon.js"))
-    </script>
-    <script>
-        tpwidget("init", {
-            "flavor": "bubble",
-            "location": "WW0V9QP93VS8",
-            "geolocation": "disabled",
-            "position": "top-left",
-            "margin": "10px 10px",
-            "language": "zh-chs",
-            "unit": "c",
-            "theme": "chameleon",
-            "uid": "U4954D65B6",
-            "hash": "4822c3f0c454c300fe8a5d6a3beb95bb"
-        });
-        tpwidget("show");
-    </script>
 
 
 
