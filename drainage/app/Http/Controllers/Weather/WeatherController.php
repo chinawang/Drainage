1c04f4cb4a34b46384ca520744acfb8a309507d5
Wangyx <chinawangyx@hotmail.com>
2017-04-14 12:16:50 +0800

    update html code

--- drainage/app/Http/Controllers/Weather/WeatherController.php
+++ drainage/app/Http/Controllers/Weather/WeatherController.php
@@ -19,11 +19,28 @@ class WeatherController extends Controller
      */
     public function showWeather()
     {
-        $url = $this->getWeatherURL();
-        $param = ['weatherURL' => $url];
+//        $url = $this->getWeatherURL();
+//        $param = ['weatherURL' => $url];
+
+        // 心知天气接口调用凭据
+        $key = 'rybl3izena2a0lug'; // 测试用 key，请更换成您自己的 Key
+        $uid = 'U4954D65B6'; // 测试用 用户ID，请更换成您自己的用户ID
+        // 参数
+        $api = 'http://api.thinkpage.cn/v3/weather/now.json'; // 接口地址
+        $location = '郑州'; // 城市名称。除拼音外，还可以使用 v3 id、汉语等形式
+        // 获取当前时间戳，并构造验证参数字符串
+        $keyname = "ts=".time()."&ttl=300&uid=".$uid;
+
+        // 使用 HMAC-SHA1 方式，以 API 密钥（key）对上一步生成的参数字符串（raw）进行加密
+        $sig = base64_encode(hash_hmac('sha1', $keyname, $key, true));
+
+        // 将上一步生成的加密结果用 base64 编码，并做一个 urlencode，得到签名sig
+        $signedkeyname = $keyname."&sig=".urlencode($sig);
+        // 最终构造出可由前端或服务端进行调用的 url
+        $url = $api."?location=".$location."&".$signedkeyname;
 
 //        return $url;
-        $weatherData = fopen($url,"r");
+        $weatherData = file_get_contents($url);
         return $weatherData;
 
         return view('weather.weather',$param);
