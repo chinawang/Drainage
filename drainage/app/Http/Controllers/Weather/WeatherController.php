a835e7094fa47fc82200f7bd6006133e42c7e7ea
Wangyx <chinawangyx@hotmail.com>
2017-04-14 11:05:58 +0800

    update html code

--- drainage/app/Http/Controllers/Weather/WeatherController.php
+++ drainage/app/Http/Controllers/Weather/WeatherController.php
@@ -29,24 +29,19 @@ class WeatherController extends Controller
         $key = 'rybl3izena2a0lug'; // 测试用 key，请更换成您自己的 Key
         $uid = 'U4954D65B6'; // 测试用 用户ID，请更换成您自己的用户ID
         // 参数
-        $api = 'https://api.thinkpage.cn/v3/weather/daily.json'; // 接口地址
+        $api = 'http://api.thinkpage.cn/v3/weather/now.json'; // 接口地址
         $location = '郑州'; // 城市名称。除拼音外，还可以使用 v3 id、汉语等形式
-        // 生成签名。文档：https://www.seniverse.com/doc#sign
-        $param = [
-            'ts' => time(),
-            'ttl' => 300,
-            'uid' => $uid,
-        ];
-        $sig_data = http_build_query($param); // http_build_query会自动进行url编码
-        // 使用 HMAC-SHA1 方式，以 API 密钥（key）对上一步生成的参数字符串（raw）进行加密，然后base64编码
-        $sig = base64_encode(hash_hmac('sha1', $sig_data, $key, TRUE));
-        // 拼接Url中的get参数。文档：https://www.seniverse.com/doc#daily
-        $param['sig'] = $sig; // 签名
-        $param['location'] = $location;
-        $param['start'] = 0; // 开始日期。0=今天天气
-        $param['days'] = 1; // 查询天数，1=只查一天
-        // 构造url
-        $url = $api . '?' . http_build_query($param);
+        // 获取当前时间戳，并构造验证参数字符串
+        $keyname = "ts=".time()."&ttl=300&uid=".$uid;
+
+        // 使用 HMAC-SHA1 方式，以 API 密钥（key）对上一步生成的参数字符串（raw）进行加密
+        $sig = base64_encode(hash_hmac('sha1', $keyname, $key, true));
+
+        // 将上一步生成的加密结果用 base64 编码，并做一个 urlencode，得到签名sig
+        $signedkeyname = $keyname."&sig=".urlencode($sig);
+        // 最终构造出可由前端或服务端进行调用的 url
+        $url = $api."?location=".$location."&".$signedkeyname;
+
         return $url;
     }
 }
