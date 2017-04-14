6fae5e021ce4006940e21ea2646139e338124213
Wangyx <chinawangyx@hotmail.com>
2017-04-14 10:07:32 +0800

    update html code

--- drainage/resources/views/weather/weather.blade.php
+++ drainage/resources/views/weather/weather.blade.php
@@ -27,42 +27,42 @@
 
 @section('javascript')
 
-    <script>(function (T, h, i, n, k, P, a, g, e) {
-            g = function () {
-                P = h.createElement(i);
-                a = h.getElementsByTagName(i)[0];
-                P.src = k;
-                P.charset = "utf-8";
-                P.async = 1;
-                a.parentNode.insertBefore(P, a)
-            };
-            T["ThinkPageWeatherWidgetObject"] = n;
-            T[n] || (T[n] = function () {
-                (T[n].q = T[n].q || []).push(arguments)
-            });
-            T[n].l = +new Date();
-            if (T.attachEvent) {
-                T.attachEvent("onload", g)
-            } else {
-                T.addEventListener("load", g, false)
-            }
-        }(window, document, "script", "tpwidget", "//widget.seniverse.com/widget/chameleon.js"))
-    </script>
-    <script>
-        tpwidget("init", {
-            "flavor": "bubble",
-            "location": "WW0V9QP93VS8",
-            "geolocation": "disabled",
-            "position": "top-left",
-            "margin": "200px 300px",
-            "language": "zh-chs",
-            "unit": "c",
-            "theme": "chameleon",
-            "uid": "U4954D65B6",
-            "hash": "53726de14b08605c08ed73005e8c9109"
-        });
-        tpwidget("show");
-    </script>
+    {{--<script>(function (T, h, i, n, k, P, a, g, e) {--}}
+            {{--g = function () {--}}
+                {{--P = h.createElement(i);--}}
+                {{--a = h.getElementsByTagName(i)[0];--}}
+                {{--P.src = k;--}}
+                {{--P.charset = "utf-8";--}}
+                {{--P.async = 1;--}}
+                {{--a.parentNode.insertBefore(P, a)--}}
+            {{--};--}}
+            {{--T["ThinkPageWeatherWidgetObject"] = n;--}}
+            {{--T[n] || (T[n] = function () {--}}
+                {{--(T[n].q = T[n].q || []).push(arguments)--}}
+            {{--});--}}
+            {{--T[n].l = +new Date();--}}
+            {{--if (T.attachEvent) {--}}
+                {{--T.attachEvent("onload", g)--}}
+            {{--} else {--}}
+                {{--T.addEventListener("load", g, false)--}}
+            {{--}--}}
+        {{--}(window, document, "script", "tpwidget", "//widget.seniverse.com/widget/chameleon.js"))--}}
+    {{--</script>--}}
+    {{--<script>--}}
+        {{--tpwidget("init", {--}}
+            {{--"flavor": "bubble",--}}
+            {{--"location": "WW0V9QP93VS8",--}}
+            {{--"geolocation": "disabled",--}}
+            {{--"position": "top-left",--}}
+            {{--"margin": "200px 300px",--}}
+            {{--"language": "zh-chs",--}}
+            {{--"unit": "c",--}}
+            {{--"theme": "chameleon",--}}
+            {{--"uid": "U4954D65B6",--}}
+            {{--"hash": "53726de14b08605c08ed73005e8c9109"--}}
+        {{--});--}}
+        {{--tpwidget("show");--}}
+    {{--</script>--}}
 
     <script src="http://ext.weather.com.cn/home/58f02dc29dfb1d0be4678dd8.js"></script>
 
