9683061c5bd384e10e812fc41aa3456415028baf
Wangyx <chinawangyx@hotmail.com>
2017-04-14 14:20:17 +0800

    update html code

--- drainage/resources/views/weather/weather.blade.php
+++ drainage/resources/views/weather/weather.blade.php
@@ -11,14 +11,14 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12 col-md-offset-0">
                 <div class="panel panel-default custom-panel">
                     <div class="panel-heading">
                         天气
                     </div>
                     <div class="panel-body custom-panel-body" id="content-weather">
                         <iframe src="//www.seniverse.com/weather/weather.aspx?uid=U4954D65B6&cid=CHHA000000&l=zh-CHS&p=SMART&a=0&u=C&s=1&m=0&x=1&d=5&fc=&bgc=&bc=&ti=0&in=1&li="
-                                frameborder="0" scrolling="no" width="360" height="300"
+                                frameborder="0" scrolling="no" width="800" height="300"
                                 allowTransparency="true"></iframe>
                     </div>
                 </div>
