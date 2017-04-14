59a00c94ac849b74d23a03988edbb272607f8a77
Wangyx <chinawangyx@hotmail.com>
2017-04-14 10:57:37 +0800

    update html code

--- drainage/resources/views/weather/weather.blade.php
+++ drainage/resources/views/weather/weather.blade.php
@@ -67,22 +67,8 @@
     <script src="https://cdn.bootcss.com/jsSHA/2.2.0/sha1.js"></script>
 
     <script>
-        /******** 本示例仅做开发参考使用，不建议在生产环节下暴露 key！ ********/
-        var UID = "ID：U4954D65B6"; // 测试用 用户ID，请更换成您自己的用户ID
-        var KEY = "rybl3izena2a0lug"; // 测试用key，请更换成您自己的 Key
-        var API = "http://api.thinkpage.cn/v3/weather/now.json"; // 获取天气实况
-        var LOCATION = "zhengzhou"; // 除拼音外，还可以使用 v3 id、汉语等形式
-        // 获取当前时间戳
-        var ts = Math.floor((new Date()).getTime() / 1000);
-        // 构造验证参数字符串
-        var str = "ts="+ts+"&uid=" + UID;
-        // 使用 HMAC-SHA1 方式，以 API 密钥（key）对上一步生成的参数字符串（raw）进行加密
-        // 并将加密结果用 base64 编码，并做一个 urlencode，得到签名 sig
-        var sig = CryptoJS.HmacSHA1(str, KEY).toString(CryptoJS.enc.Base64);
-        sig = encodeURIComponent(sig);
-        str = str + "&sig=" + sig;
-        // 构造最终请求的 url
-        var url = API + "?location=" + LOCATION + "&" + str + "&callback=?";
+
+        var url = "{{ $weatherURL }}";
         // 直接发送请求进行调用，手动处理回调函数
         $.getJSON(url, function(data) {
             var obj = document.getElementById('content-weather');
