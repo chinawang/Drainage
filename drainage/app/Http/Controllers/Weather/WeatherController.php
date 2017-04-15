c55e4155f7d2acd63122ad6c7b55673d3e339164
Wangyx <chinawangyx@hotmail.com>
2017-04-15 22:29:53 +0800

    update html code

--- drainage/app/Http/Controllers/Weather/WeatherController.php
+++ drainage/app/Http/Controllers/Weather/WeatherController.php
@@ -194,7 +194,7 @@ class WeatherController extends Controller
         $uid = 'U4954D65B6'; // 测试用 用户ID，请更换成您自己的用户ID
         // 参数
         $api = 'http://api.thinkpage.cn/v3/weather/now.json'; // 接口地址
-        $location = 'zhengzhou'; // 城市名称。除拼音外，还可以使用 v3 id、汉语等形式
+        $location = 'suqian'; // 城市名称。除拼音外，还可以使用 v3 id、汉语等形式
         // 获取当前时间戳，并构造验证参数字符串
         $keyname = "ts=".time()."&ttl=300&uid=".$uid;
 
@@ -216,7 +216,7 @@ class WeatherController extends Controller
         $uid = 'U4954D65B6'; // 测试用 用户ID，请更换成您自己的用户ID
         // 参数
         $api = 'http://api.thinkpage.cn/v3/weather/daily.json'; // 接口地址
-        $location = 'zhengzhou'; // 城市名称。除拼音外，还可以使用 v3 id、汉语等形式
+        $location = 'suqian'; // 城市名称。除拼音外，还可以使用 v3 id、汉语等形式
         // 获取当前时间戳，并构造验证参数字符串
         $keyname = "ts=".time()."&ttl=300&uid=".$uid;
 
