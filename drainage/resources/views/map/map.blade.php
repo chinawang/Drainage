4930e9581f3edcfaef1844f8172b9ff850991e25
Wangyx <chinawangyx@hotmail.com>
2017-04-16 17:40:59 +0800

    update html code

--- drainage/resources/views/map/map.blade.php
+++ drainage/resources/views/map/map.blade.php
@@ -43,17 +43,18 @@
 @section('javascript')
 
     <!--引入高德地图JSAPI -->
-    <script type="text/javascript" src="http://webapi.amap.com/maps?v=1.3&key=be195b71e866e63bb1688b420135565c"></script>
+    <script type="text/javascript"
+            src="http://webapi.amap.com/maps?v=1.3&key=be195b71e866e63bb1688b420135565c"></script>
     <!--引入UI组件库（1.0版本） -->
     <script src="//webapi.amap.com/ui/1.0/main.js"></script>
 
     <script type="text/javascript">
 
         //创建地图
-        var map = new AMap.Map('map-container',{
+        var map = new AMap.Map('map-container', {
             zoom: 13,
-            center:[113.658578,34.746427],
-            mapStyle:'normal',
+            center: [113.658578, 34.746427],
+            mapStyle: 'normal',
             resizeEnable: true
         });
 
@@ -61,42 +62,47 @@
         AMapUI.setDomLibrary($);
 
         //加载BasicControl，loadUI的路径参数为模块名中 'ui/' 之后的部分
-        AMapUI.loadUI(['control/BasicControl'], function(BasicControl) {
+        AMapUI.loadUI(['control/BasicControl'], function (BasicControl) {
 
             //缩放控件
             map.addControl(new BasicControl.Zoom({
-                position: 'rb' //right bottom，右下角
+                position: 'rt' //right top，右上角
             }));
 
         });
 
         //标记位置
-        var lngLats=[
-            [113.655831,34.805647],[113.69394,34.779145],
-            [113.606393,34.763634],[113.684671,34.732885],
-            [113.610856,34.735142],[113.652398,34.754326],
-            [113.637635,34.781118],[113.679864,34.698456]
+        var lngLats = [
+            [113.655831, 34.805647], [113.69394, 34.779145],
+            [113.606393, 34.763634], [113.684671, 34.732885],
+            [113.610856, 34.735142], [113.652398, 34.754326],
+            [113.637635, 34.781118], [113.679864, 34.698456]
         ];
 
-//        //创建标记位置
-//        for(var i = 0, marker; i < lngLats.length; i++){
-//            marker=new AMap.Marker({
-//                position:lnglats[i],
-//                map:map
-//            });
-//        }
+        //        //创建标记位置
+        //        for(var i = 0, marker; i < lngLats.length; i++){
+        //            marker=new AMap.Marker({
+        //                position:lnglats[i],
+        //                map:map
+        //            });
+        //        }
+
+        var defaultIconStyle = 'darkblue', //默认的图标样式
+                hoverIconStyle = 'blue', //鼠标hover时的样式
+                selectedIconStyle = 'lightblue' //选中时的图标样式
+                ;
 
         //加载SimpleMarker
-        AMapUI.loadUI(['overlay/SimpleMarker'], function(SimpleMarker) {
+        AMapUI.loadUI(['overlay/SimpleMarker', 'overlay/SimpleInfoWindow'], function (SimpleMarker, SimpleInfoWindow) {
 
             //内置的样式
             var iconStyles = SimpleMarker.getBuiltInIconStyles();
 
             for (var i = 0, len = lngLats.length; i < len; i++) {
 
-               var marker = new SimpleMarker({
+                var marker = new SimpleMarker({
                     //使用内置的iconStyle
-                    iconStyle: 'darkblue',
+                    iconStyle: defaultIconStyle,
 
                     //图标文字
                     iconLabel: {
@@ -121,22 +127,43 @@
                     }
                 });
 
+                var infoWindow = new SimpleInfoWindow({
+
+                    infoTitle: '<strong>这里是标题</strong>',
+                    infoBody: '<p class="my-desc"><strong>这里是内容。</strong> <br/> 高德地图 JavaScript API，是由 JavaScript 语言编写的应用程序接口，' +
+                    '它能够帮助您在网站或移动端中构建功能丰富、交互性强的地图应用程序</p>',
+
+                    //基点指向marker的头部位置
+                    offset: new AMap.Pixel(0, -31)
+                });
+
+                function openInfoWin() {
+                    infoWindow.open(map, marker.getPosition());
+                }
+
                 marker.emit('mouseover', {target: marker});
 
-                marker.on('mouseover', function(e) {
-                    e.target.setIconStyle('blue');
+                marker.on('mouseover', function (e) {
+                    e.target.setIconStyle(hoverIconStyle);
                 });
 
                 marker.emit('mouseout', {target: marker});
 
-                marker.on('mouseout', function(e) {
-                    e.target.setIconStyle('darkblue');
+                marker.on('mouseout', function (e) {
+                    e.target.setIconStyle(defaultIconStyle);
+                });
+
+                marker.emit('click', {target: marker});
+
+                marker.on('click', function (e) {
+                    e.target.setIconStyle(selectedIconStyle);
+                    openInfoWin();
                 });
             }
 
         });
 
-        map.setFeatures(['road','bg'])//要素显示:道路、背景
+        map.setFeatures(['road', 'bg'])//要素显示:道路、背景
 
         map.setFitView();
 
