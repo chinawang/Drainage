231eb5e4988a4142ecc16bb575b87af3296d77d0
Wangyx <chinawangyx@hotmail.com>
2017-04-20 15:20:10 +0800

    update map code

--- drainage/resources/views/map/map.blade.php
+++ drainage/resources/views/map/map.blade.php
@@ -147,7 +147,19 @@
 
                 marker.emit('mouseover', {target: marker});
 
-                var contentHtml = '<div class="row" style="width: 260px;margin: 10px 0;"><div class="col-md-3 col-md-offset-0"><img src="/img/map/dot_yellow.png" style="width: 32px;height: 32px;"></div><div class="col-md-6 col-md-offset-0" style="margin-left: -5px"><div style="font-size: 14px;color:#4a4a4a">已启动泵组: 3组</div><div style="font-size: 14px;color:#4a4a4a">未启动泵组: 2组</div></div><div class="col-md-3 col-md-offset-0"><a href="/station/lists" class="btn-link" style="font-size: 12px;height: 40px;line-height: 40px">详情</a></div></div>';
+                var contentHtml = '<div class="row" style="width: 260px;margin: 10px 0;">' +
+                        '<div class="col-md-3 col-md-offset-0">' +
+                        '<img src="/img/map/dot_yellow.png" style="width: 32px;height: 32px;">' +
+                        '</div>' +
+                        '<div class="col-md-6 col-md-offset-0" style="margin-left: -5px">' +
+                        '<div style="font-size: 14px;color:#4a4a4a">已启动泵组: 3组</div>' +
+                        '<div style="font-size: 14px;color:#4a4a4a">未启动泵组: 2组</div>' +
+                        '<div style="font-size: 14px;color:#4a4a4a">水位: 2.8米</div>' +
+                        '</div>' +
+                        '<div class="col-md-3 col-md-offset-0">' +
+                        '<a href="/station/lists" class="btn-link" style="font-size: 12px;height: 40px;line-height: 40px">详情</a>' +
+                        '</div>' +
+                        '</div>';
 
                 marker.on('mouseover', function (e) {
                     e.target.setIconStyle(hoverIconStyle);
