86ec288b7c9651f573440402531ca046c206f576
Wangyx <chinawangyx@hotmail.com>
2017-04-23 17:09:42 +0800

    update stationRun code

--- drainage/resources/views/station/runDetail.blade.php
+++ drainage/resources/views/station/runDetail.blade.php
@@ -309,7 +309,7 @@
                     var data = [],
                             time = (new Date()).getTime(),
                             i;
-                    for (i = -119; i <= 0; i += 1) {
+                    for (i = -599; i <= 0; i += 1) {
                         data.push({
                             x: time + i * 1000,
                             y: Math.random() * 5
