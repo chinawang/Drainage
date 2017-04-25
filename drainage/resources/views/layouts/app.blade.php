4328939467b30aa1a4868d7dc04c222f8f3b0524
Wangyx <chinawangyx@hotmail.com>
2017-04-25 15:38:53 +0800

    update stationRun code

--- drainage/resources/views/layouts/app.blade.php
+++ drainage/resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top navbar-custom">
+        <nav class="navbar navbar-default navbar-static-top" style="background-color: #1296db;">
             <div class="container">
                 <div class="navbar-header">
 
