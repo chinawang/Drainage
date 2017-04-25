a03d61b9f42eede718d300bf04c63158c23c0af8
Wangyx <chinawangyx@hotmail.com>
2017-04-25 15:33:48 +0800

    update stationRun code

--- drainage/resources/views/home.blade.php
+++ drainage/resources/views/home.blade.php
@@ -17,73 +17,73 @@
                     <ul class="main-menu">
                         <li class="station">
                             <a href="/station/lists" class="menu-item">
-                                <img class="menu-icon" src="/img/home/station_green.png">
+                                <img class="menu-icon" src="/img/home/station_blue.png">
                                 <h4>泵站资料管理</h4>
                             </a>
                         </li>
                         <li class="equipment">
                             <a href="/equipment/lists" class="menu-item">
-                                <img class="menu-icon" src="/img/home/equipment_green.png">
+                                <img class="menu-icon" src="/img/home/equipment_blue.png">
                                 <h4>设备资料管理</h4>
                             </a>
                         </li>
                         <li class="failure">
                             <a href="/failure/lists" class="menu-item">
-                                <img class="menu-icon" src="/img/home/failure_green.png">
+                                <img class="menu-icon" src="/img/home/failure_blue.png">
                                 <h4>设备维修管理</h4>
                             </a>
                         </li>
                         <li class="map">
                             <a href="/map/view" class="menu-item">
-                                <img class="menu-icon" src="/img/home/map_green.png">
+                                <img class="menu-icon" src="/img/home/map_blue.png">
                                 <h4>泵站地图</h4>
                             </a>
                         </li>
                         <li class="status">
                             <a href="/station/runList" class="menu-item">
-                                <img class="menu-icon" src="/img/home/status_green.png">
+                                <img class="menu-icon" src="/img/home/status_blue.png">
                                 <h4>泵站工作状态</h4>
                             </a>
                         </li>
                         <li class="station-warning">
                             <a href="" class="menu-item">
-                                <img class="menu-icon" src="/img/home/warning_green.png">
+                                <img class="menu-icon" src="/img/home/warning_blue.png">
                                 <h4>泵站报警管理</h4>
                             </a>
                         </li>
                         <li class="report">
                             <a href="" class="menu-item">
-                                <img class="menu-icon" src="/img/home/report_green.png">
+                                <img class="menu-icon" src="/img/home/report_blue.png">
                                 <h4>统计报表</h4>
                             </a>
                         </li>
                         <li class="weather">
                             <a href="/weather/view" class="menu-item">
-                                <img class="menu-icon" src="/img/home/weather_green.png">
+                                <img class="menu-icon" src="/img/home/weather_blue.png">
                                 <h4>天气预报</h4>
                             </a>
                         </li>
                         <li class="user">
                             <a href="/user/lists" class="menu-item">
-                                <img class="menu-icon" src="/img/home/user_green.png">
+                                <img class="menu-icon" src="/img/home/user_blue.png">
                                 <h4>账户管理</h4>
                             </a>
                         </li>
                         <li class="role">
                             <a href="/role/lists" class="menu-item">
-                                <img class="menu-icon" src="/img/home/role_green.png">
+                                <img class="menu-icon" src="/img/home/role_blue.png">
                                 <h4>角色管理</h4>
                             </a>
                         </li>
                         <li href="permission">
                             <a href="/permission/lists" class="menu-item">
-                                <img class="menu-icon" src="/img/home/permission_green.png">
+                                <img class="menu-icon" src="/img/home/permission_blue.png">
                                 <h4>行为权限管理</h4>
                             </a>
                         </li>
                         <li class="log">
                             <a href="" class="menu-item">
-                                <img class="menu-icon" src="/img/home/log_green.png">
+                                <img class="menu-icon" src="/img/home/log_blue.png">
                                 <h4>系统日志管理</h4>
                             </a>
                         </li>
