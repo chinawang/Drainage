4f9ac3a55614521ba91de65b8df21c625834bb32
Wangyx <chinawangyx@hotmail.com>
2017-05-20 21:27:24 +0800

    add report code

--- drainage/resources/views/report/reportList.blade.php
+++ drainage/resources/views/report/reportList.blade.php
@@ -27,14 +27,15 @@
         <div class="row">
             <div class="col-md-12 col-md-offset-0">
                 <div class="panel panel-default custom-panel">
-                    <div class="panel-heading">
+                    {{--<div class="panel-heading">--}}
+                       {{----}}
+                    {{--</div>--}}
+                    <div class="panel-body custom-panel-body">
                         <ul class="nav nav-tabs">
                             <li class="active"><a href="#home" data-toggle="tab" aria-expanded="false">水位统计</a></li>
                             <li class=""><a href="#profile" data-toggle="tab" aria-expanded="true">设备运行统计</a></li>
 
                         </ul>
-                    </div>
-                    <div class="panel-body custom-panel-body">
                         <div id="myTabContent" class="tab-content">
                             <div class="tab-pane fade active in" id="home">
                                 <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
