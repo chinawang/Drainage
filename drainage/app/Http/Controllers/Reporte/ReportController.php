39e1a0acd45ab567dd5ae713fab0cd416fb1dcba
Wangyx <chinawangyx@hotmail.com>
2017-05-22 21:38:49 +0800

    add report code

--- drainage/app/Http/Controllers/Reporte/ReportController.php
+++ drainage/app/Http/Controllers/Reporte/ReportController.php
@@ -2,13 +2,191 @@
 
 namespace App\Http\Controllers\Reporte;
 
+use App\Http\Logic\Maintenance\FailureLogic;
+use App\Http\Logic\Maintenance\MaintenanceLogic;
+use App\Http\Logic\Station\EquipmentLogic;
+use App\Http\Logic\Station\StationLogic;
+use App\Http\Logic\User\UserLogic;
+use App\Http\Validations\Station\StationValidation;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ReportController extends Controller
 {
-    public function showReport()
+
+    /**
+     * @var StationLogic
+     */
+    protected $stationLogic;
+
+    /**
+     * @var StationValidation
+     */
+    protected $stationValidation;
+
+    /**
+     * @var FailureLogic
+     */
+    protected $failureLogic;
+
+    /**
+     * @var EquipmentLogic
+     */
+    protected $equipmentLogic;
+
+    /**
+     * @var UserLogic
+     */
+    protected $userLogic;
+
+    /**
+     * @var MaintenanceLogic
+     */
+    protected $maintenanceLogic;
+
+    /**
+     * ReportController constructor.
+     * @param StationLogic $stationLogic
+     * @param StationValidation $stationValidation
+     * @param EquipmentLogic $equipmentLogic
+     * @param UserLogic $userLogic
+     * @param FailureLogic $failureLogic
+     * @param MaintenanceLogic $maintenanceLogic
+     */
+    public function __construct(StationLogic $stationLogic, StationValidation $stationValidation,
+                                EquipmentLogic $equipmentLogic, UserLogic $userLogic,
+                                FailureLogic $failureLogic, MaintenanceLogic $maintenanceLogic)
     {
-        return view('report.reportList');
+        $this->middleware('auth');
+
+        $this->stationLogic = $stationLogic;
+        $this->stationValidation = $stationValidation;
+        $this->equipmentLogic = $equipmentLogic;
+        $this->userLogic = $userLogic;
+        $this->failureLogic = $failureLogic;
+        $this->maintenanceLogic = $maintenanceLogic;
     }
+
+    public function showReport($stationID)
+    {
+        $stationNum = "1";
+        if($stationID != null)
+        {
+            $stationTemp = $this->stationInfo($stationID);
+            $stationNum = $stationTemp['station_number'];
+        }
+
+        $stations = $this->stationList();
+
+        $input = $this->stationValidation->stationPaginate();
+
+        $cursorPage      = array_get($input, 'cursor_page', null);
+        $pageSize        = array_get($input, 'page_size', 20);
+        $orderColumn     = array_get($input, 'order_column', 'created_at');
+        $orderDirection  = array_get($input, 'order_direction', 'asc');
+
+        $stationRTPaginate = $this->getStationRTList($stationNum,$pageSize,$cursorPage);
+
+        // 故障统计
+
+        $failurePaginate = $this->failureLogic->getFailures($pageSize,$orderColumn,$orderDirection,$cursorPage);
+
+        foreach($failurePaginate as $failure)
+        {
+            $equipment = $this->equipmentInfo($failure['equipment_id']);
+            $station = $this->stationInfo($failure['station_id']);
+            $reporter = $this->userInfo($failure['reporter_id']);
+            $repairer = $this->userInfo($failure['repairer_id']);
+
+            $failure['equipment_name'] = $equipment['name'];
+            $failure['station_name'] = $station['name'];
+            $failure['reporter_name'] = $reporter['realname'];
+            $failure['repairer_name'] = $repairer['realname'];
+        }
+
+
+        // 维修统计
+
+        $maintenancePaginate = $this->maintenanceLogic->getMaintenances($pageSize,$orderColumn,$orderDirection,$cursorPage);
+
+        foreach($maintenancePaginate as $maintenance)
+        {
+            $equipment = $this->equipmentInfo($maintenance['equipment_id']);
+            $station = $this->stationInfo($maintenance['station_id']);
+            $repairer = $this->userInfo($maintenance['repairer_id']);
+
+            $maintenance['equipment_name'] = $equipment['name'];
+            $maintenance['station_name'] = $station['name'];
+            $maintenance['repairer_name'] = $repairer['realname'];
+        }
+
+        $param = ['stations' => $stations,'waterList' => $stationRTPaginate,
+            'runList' => $stationRTPaginate,'statusList' => $stationRTPaginate,
+            'failures' => $failurePaginate,'maintenances' => $maintenancePaginate,];
+
+        return view('report.reportList',$param);
+    }
+
+    public function getStationRTList($stationNum, $size, $cursorPage)
+    {
+        $stationTable = "stationRT_".$stationNum;
+        $stationRTList = DB::select('select * from '.$stationTable.' order by Time asc')->paginate($size, $columns = ['*'], $pageName = 'page', $cursorPage);
+
+        return $stationRTList;
+    }
+
+    public function stationList()
+    {
+        $stations = $this->stationLogic->getAllStations();
+
+        return $stations;
+    }
+
+    /**
+     * @param $failureID
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function failureInfo($failureID)
+    {
+        $failure = $this->failureLogic->findFailure($failureID);
+        return $failure;
+    }
+
+    /**
+     * 查询设备信息
+     *
+     * @param $equipmentID
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function equipmentInfo($equipmentID)
+    {
+        $equipment = $this->equipmentLogic->findEquipment($equipmentID);
+        return $equipment;
+    }
+
+    /**
+     * 查询泵站信息
+     *
+     * @param $stationID
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function stationInfo($stationID)
+    {
+        $station = $this->stationLogic->findStation($stationID);
+        return $station;
+    }
+
+    /**
+     * 查询人员信息
+     *
+     * @param $userID
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function userInfo($userID)
+    {
+        $user = $this->userLogic->findUser($userID);
+        return $user;
+    }
+
+
 }
--- drainage/resources/views/report/reportList.blade.php
+++ drainage/resources/views/report/reportList.blade.php
@@ -28,36 +28,268 @@
             <div class="col-md-12 col-md-offset-0">
                 <div class="panel panel-default custom-panel">
                     {{--<div class="panel-heading">--}}
-                       {{----}}
+                    {{----}}
                     {{--</div>--}}
                     <div class="panel-body custom-panel-body">
                         <ul class="nav nav-tabs">
                             <li class="active"><a href="#water" data-toggle="tab" aria-expanded="false">泵站水位统计</a></li>
                             <li class=""><a href="#run" data-toggle="tab" aria-expanded="true">设备运行统计</a></li>
+                            <li class=""><a href="#status" data-toggle="tab" aria-expanded="true">设备启停统计</a></li>
                             <li class=""><a href="#fail" data-toggle="tab" aria-expanded="true">设备故障统计</a></li>
                             <li class=""><a href="#mantenance" data-toggle="tab" aria-expanded="true">设备维修统计</a></li>
 
                         </ul>
                         <div id="myTabContent" class="tab-content" style="margin-top: 20px">
-                            <div class="tab-pane fade active in" id="water">
-                                <div class="well" style="text-align: center; padding: 100px;">
-                                    暂无内容
+
+
+                            <form class="form-horizontal" role="form" method="POST" action="">
+                                {{ csrf_field() }}
+
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="station" class="col-md-4 control-label">选择泵站:</label>
+
+                                            <div class="col-md-6">
+                                                <select class="form-control" id="select" name="station_id">
+                                                    {{--<option value="" selected="selected" style="display: none">选择泵站</option>--}}
+                                                    @foreach ($stations as $station)
+                                                        <option value="{{ $station['id'] }}">{{ $station['name'] }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="repair_at" class="col-md-4 control-label">时间范围:</label>
+
+                                            <div class="col-md-6">
+                                                <input type="text" class="form-control" id="datetimepicker" name="time"
+                                                       value="" placeholder="起始时间" required>
+                                                <em>-</em>
+                                                <input type="text" class="form-control" id="datetimepicker" name="time"
+                                                       value="" placeholder="结束时间" required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <div class="col-md-6 col-md-offset-4">
+                                                <button type="submit" class="btn btn-primary btn-custom">
+                                                    查询
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
+                            </form>
+
+                            <div class="tab-pane fade active in" id="water">
+                                @if (!empty($waterList[0]))
+                                    <table class="table table-hover table-bordered ">
+                                        <thead>
+                                        <tr>
+                                            <th>时间</th>
+                                            <th>泵站名称</th>
+                                            <th>涵洞水位</th>
+                                            <th>集水池水位</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        @foreach ($waterList as $water)
+                                            <tr>
+                                                <td>{{ $water['Time'] }}</td>
+                                                <td>{{ $water['name'] }}</td>
+                                                <td>{{ $water['culvertWater'] }}</td>
+                                                <td>{{ $water['tankWater'] }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                    <div class="table-pagination">
+                                        {!! $waterList->render() !!}
+                                    </div>
+                                @else
+                                    <div class="well" style="text-align: center; padding: 100px;">
+                                        暂无内容
+                                    </div>
+                                @endif
                             </div>
                             <div class="tab-pane fade" id="run">
-                                <div class="well" style="text-align: center; padding: 100px;">
-                                    暂无内容
-                                </div>
+                                @if (!empty($runList[0]))
+                                    <table class="table table-hover table-bordered ">
+                                        <thead>
+                                        <tr>
+                                            <th>时间</th>
+                                            <th>泵站名称</th>
+                                            <th>1号泵电流</th>
+                                            <th>2号泵电流</th>
+                                            <th>3号泵电流</th>
+                                            <th>4号泵电流</th>
+                                            <th>5号泵电流</th>
+                                            <th>AB相线电压</th>
+                                            <th>BC相线电压</th>
+                                            <th>CA相线电压</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        @foreach ($runList as $run)
+                                            <tr>
+                                                <td>{{ $run['Time'] }}</td>
+                                                <td>{{ $run['name'] }}</td>
+                                                <td>{{ $run['pumpCurrent1'] }}</td>
+                                                <td>{{ $run['pumpCurrent2'] }}</td>
+                                                <td>{{ $run['pumpCurrent3'] }}</td>
+                                                <td>{{ $run['pumpCurrent4'] }}</td>
+                                                <td>{{ $run['pumpCurrent5'] }}</td>
+                                                <td>{{ $run['pumpVoltageAB'] }}</td>
+                                                <td>{{ $run['pumpVoltageBC'] }}</td>
+                                                <td>{{ $run['pumpVoltageCA'] }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                    <div class="table-pagination">
+                                        {!! $runList->render() !!}
+                                    </div>
+                                @else
+                                    <div class="well" style="text-align: center; padding: 100px;">
+                                        暂无内容
+                                    </div>
+                                @endif
+                            </div>
+                            <div class="tab-pane fade" id="status">
+                                @if (!empty($statusList[0]))
+                                    <table class="table table-hover table-bordered ">
+                                        <thead>
+                                        <tr>
+                                            <th>时间</th>
+                                            <th>泵站名称</th>
+                                            <th>1号泵启停状态</th>
+                                            <th>2号泵启停状态</th>
+                                            <th>3号泵启停状态</th>
+                                            <th>4号泵启停状态</th>
+                                            <th>1号格栅启停状态</th>
+                                            <th>2号格栅启停状态</th>
+                                            <th>绞龙启停状态</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        @foreach ($statusList as $status)
+                                            <tr>
+                                                <td>{{ $status['Time'] }}</td>
+                                                <td>{{ $status['name'] }}</td>
+                                                <td>{{ $status['pumpStatus1'] }}</td>
+                                                <td>{{ $status['pumpStatus2'] }}</td>
+                                                <td>{{ $status['pumpStatus3'] }}</td>
+                                                <td>{{ $status['pumpStatus4'] }}</td>
+                                                <td>{{ $status['removerStatus1'] }}</td>
+                                                <td>{{ $status['removerStatus2'] }}</td>
+                                                <td>{{ $status['augerStatus'] }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                    <div class="table-pagination">
+                                        {!! $statusList->render() !!}
+                                    </div>
+                                @else
+                                    <div class="well" style="text-align: center; padding: 100px;">
+                                        暂无内容
+                                    </div>
+                                @endif
                             </div>
                             <div class="tab-pane fade" id="fail">
-                                <div class="well" style="text-align: center; padding: 100px;">
-                                    暂无内容
-                                </div>
+                                @if (!empty($failures[0]))
+                                    <table class="table table-hover table-bordered ">
+                                        <thead>
+                                        <tr>
+                                            <th>报修时间</th>
+                                            <th>所属泵站</th>
+                                            <th>故障设备</th>
+                                            <th>故障类型</th>
+                                            <th>故障描述</th>
+                                            <th>设备状态</th>
+                                            <th>报修人</th>
+                                            <th>维修进度</th>
+                                            <th hidden="hidden">维修人</th>
+                                            <th hidden="hidden">维修时间</th>
+
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach ($failures as $failure)
+                                            <tr>
+                                                <td>{{ $failure['report_at'] }}</td>
+                                                <td>{{ $failure['station_name'] }}</td>
+                                                <td>{{ $failure['equipment_name'] }}</td>
+                                                <td>{{ $failure['failure_type'] }}</td>
+                                                <td>{{ $failure['failure_description'] }}</td>
+                                                <td>{{ $failure['equipment_status'] }}</td>
+                                                <td>{{ $failure['reporter_name'] }}</td>
+                                                <td>
+                                                    @if($failure['repair_process'] == 0)
+                                                        未维修
+                                                    @elseif($failure['repair_process'] == 1)
+                                                        维修中
+                                                    @elseif($failure['repair_process'] == 2)
+                                                        维修完成
+                                                    @endif
+                                                </td>
+                                                <td hidden="hidden">{{ $failure['repairer_name'] }}</td>
+                                                <td hidden="hidden">{{ $failure['repair_at'] }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                    <div class="table-pagination">
+                                        {!! $failures->render() !!}
+                                    </div>
+                                @else
+                                    <div class="well" style="text-align: center; padding: 100px;">
+                                        暂无内容
+                                    </div>
+                                @endif
                             </div>
                             <div class="tab-pane fade" id="mantenance">
-                                <div class="well" style="text-align: center; padding: 100px;">
-                                    暂无内容
-                                </div>
+                                @if (!empty($maintenances[0]))
+                                    <table class="table table-hover table-bordered ">
+                                        <thead>
+                                        <tr>
+                                            <th>维修时间</th>
+                                            <th>所属泵站</th>
+                                            <th>故障设备</th>
+                                            <th>故障原因</th>
+                                            <th>解决办法</th>
+                                            <th>维修人</th>
+
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach ($maintenances as $maintenance)
+                                            <tr>
+                                                <td>{{ $maintenance['repair_at'] }}</td>
+                                                <td>{{ $maintenance['station_name'] }}</td>
+                                                <td>{{ $maintenance['equipment_name'] }}</td>
+                                                <td>{{ $maintenance['failure_reason'] }}</td>
+                                                <td>{{ $maintenance['repair_solution'] }}</td>
+                                                <td>{{ $maintenance['repairer_name'] }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                    <div class="table-pagination">
+                                        {!! $maintenances->render() !!}
+                                    </div>
+                                @else
+                                    <div class="well" style="text-align: center; padding: 100px;">
+                                        暂无内容
+                                    </div>
+                                @endif
                             </div>
 
                         </div>
