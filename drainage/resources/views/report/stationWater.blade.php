0a4d0857e8d225de8d1f32e77744a28bb599b4ce
Wangyx <chinawangyx@hotmail.com>
2017-05-23 17:23:18 +0800

    add report code

--- drainage/resources/views/report/stationWater.blade.php
+++ drainage/resources/views/report/stationWater.blade.php
@@ -31,7 +31,7 @@
                     {{----}}
                     {{--</div>--}}
                     <div class="panel-body custom-panel-body">
-                        <div class="card">
+                        <div class="row">
                             <ul class="nav nav-tabs">
                                 <li class="active"><a href="/report/stationWater">泵站水位统计</a></li>
                                 <li class=""><a href="/report/stationRunning">设备运行统计</a></li>
