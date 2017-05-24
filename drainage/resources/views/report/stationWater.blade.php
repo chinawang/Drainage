92993af4ee5cf4ccfe274624f43bcc43a64ac1a7
Wangyx <chinawangyx@hotmail.com>
2017-05-24 12:32:31 +0800

    add some report chart code

--- drainage/resources/views/report/stationWater.blade.php
+++ drainage/resources/views/report/stationWater.blade.php
@@ -179,6 +179,30 @@
         });
     </script>
 
+    <script>
+        Date.prototype.format = function(format) {
+            var date = {
+                "M+": this.getMonth() + 1,
+                "d+": this.getDate(),
+                "h+": this.getHours(),
+                "m+": this.getMinutes(),
+                "s+": this.getSeconds(),
+                "q+": Math.floor((this.getMonth() + 3) / 3),
+                "S+": this.getMilliseconds()
+            };
+            if (/(y+)/i.test(format)) {
+                format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+            }
+            for (var k in date) {
+                if (new RegExp("(" + k + ")").test(format)) {
+                    format = format.replace(RegExp.$1, RegExp.$1.length == 1
+                            ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+                }
+            }
+            return format;
+        }
+    </script>
+
     <script type="text/javascript">
 
         function getStationRTHistory() {
@@ -206,7 +230,7 @@
         var datas2 = [];
 
         $.each(stationRTHistory,function(i,n){
-            categories[i] = n["Time"].Format("MM-dd HH:mm");
+            categories[i] = n["Time"].format('MM-dd h:m');
             datas1[i] = n["ywhandong"];
             datas2[i] = n["ywjishui"];
         });
