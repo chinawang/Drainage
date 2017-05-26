85b82d85d1d213b668209530eb121854425ef244
Wangyx <chinawangyx@hotmail.com>
2017-05-26 23:17:19 +0800

    update maintenance code

--- drainage/resources/views/user/list.blade.php
+++ drainage/resources/views/user/list.blade.php
@@ -145,8 +145,11 @@
         //加载状态为complete时移除loading效果
         function completeLoading() {
             if (document.readyState == "complete") {
-                var loadingMask = document.getElementById('loadingDiv');
-                loadingMask.parentNode.removeChild(loadingMask);
+                setTimeout(function () {
+                    var loadingMask = document.getElementById('loadingDiv');
+                    loadingMask.parentNode.removeChild(loadingMask);
+                },2000);
+
             }
         }
     </script>
