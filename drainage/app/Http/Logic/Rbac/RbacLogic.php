474abb5b46578a8aa86aa213d9d97bb68698d5b0
Wangyx <chinawangyx@hotmail.com>
2017-06-03 22:47:30 +0800

    update rbac code

--- drainage/app/Http/Logic/Rbac/RbacLogic.php
+++ drainage/app/Http/Logic/Rbac/RbacLogic.php
@@ -67,7 +67,7 @@ class RbacLogic extends Logic
         // role
         $userRoles = $this->getUserRoles($uid);
 
-        echo implode(",",$userRoles);
+        return $userRoles;
 
         // super administrator
         if (in_array(1, $userRoles)) return true;
