5c5c8259de57a76259a85664c27f84e9e68c7af7
Wangyx <chinawangyx@hotmail.com>
2017-06-03 12:16:13 +0800

    update map code

--- drainage/resources/views/report/stationWater.blade.php
+++ drainage/resources/views/report/stationWater.blade.php
@@ -270,32 +270,32 @@
     </script>
 
     <!--Loading-->
-    <script>
-        //获取浏览器页面可见高度和宽度
-        var _PageHeight = document.documentElement.clientHeight,
-                _PageWidth = document.documentElement.clientWidth;
-        //计算loading框距离顶部和左部的距离（loading框的宽度为215px，高度为61px）
-        var _LoadingTop = _PageHeight > 61 ? (_PageHeight - 61) / 2 : 0,
-                _LoadingLeft = _PageWidth > 215 ? (_PageWidth - 215) / 2 : 0;
-        //在页面未加载完毕之前显示的loading Html自定义内容
-        var _LoadingHtml = '<div id="loadingDiv" style="position:absolute;left:0;width:100%;height:' + _PageHeight + 'px;top:0;background:#aaa;opacity:0.8;filter:alpha(opacity=80);z-index:10000;"><div style="position: absolute; cursor1: wait; left: ' + _LoadingLeft + 'px; top:' + _LoadingTop + 'px; width: auto; height: 57px; line-height: 57px; padding-left: 50px; padding-right: 20px; background: #ffffff url(/img/loading.gif) no-repeat scroll 20px 20px; color: #696969; font-family:\'Microsoft YaHei\';box-shadow: 0px 0px 20px rgba(0, 0, 0, .08);    border: 2px solid transparent;border-radius: 4px;">页面加载中，请等待...</div></div>';
-        //呈现loading效果
-        document.write(_LoadingHtml);
-        //window.onload = function () {
-        //  var loadingMask = document.getElementById('loadingDiv');
-        //  loadingMask.parentNode.removeChild(loadingMask);
-        //};
-        //监听加载状态改变
-        document.onreadystatechange = completeLoading;
-        //加载状态为complete时移除loading效果
-        function completeLoading() {
-            if (document.readyState == "complete") {
-                var loadingMask = document.getElementById('loadingDiv');
-                loadingMask.parentNode.removeChild(loadingMask);
+    {{--<script>--}}
+        {{--//获取浏览器页面可见高度和宽度--}}
+        {{--var _PageHeight = document.documentElement.clientHeight,--}}
+                {{--_PageWidth = document.documentElement.clientWidth;--}}
+        {{--//计算loading框距离顶部和左部的距离（loading框的宽度为215px，高度为61px）--}}
+        {{--var _LoadingTop = _PageHeight > 61 ? (_PageHeight - 61) / 2 : 0,--}}
+                {{--_LoadingLeft = _PageWidth > 215 ? (_PageWidth - 215) / 2 : 0;--}}
+        {{--//在页面未加载完毕之前显示的loading Html自定义内容--}}
+        {{--var _LoadingHtml = '<div id="loadingDiv" style="position:absolute;left:0;width:100%;height:' + _PageHeight + 'px;top:0;background:#aaa;opacity:0.8;filter:alpha(opacity=80);z-index:10000;"><div style="position: absolute; cursor1: wait; left: ' + _LoadingLeft + 'px; top:' + _LoadingTop + 'px; width: auto; height: 57px; line-height: 57px; padding-left: 50px; padding-right: 20px; background: #ffffff url(/img/loading.gif) no-repeat scroll 20px 20px; color: #696969; font-family:\'Microsoft YaHei\';box-shadow: 0px 0px 20px rgba(0, 0, 0, .08);    border: 2px solid transparent;border-radius: 4px;">页面加载中，请等待...</div></div>';--}}
+        {{--//呈现loading效果--}}
+        {{--document.write(_LoadingHtml);--}}
+        {{--//window.onload = function () {--}}
+        {{--//  var loadingMask = document.getElementById('loadingDiv');--}}
+        {{--//  loadingMask.parentNode.removeChild(loadingMask);--}}
+        {{--//};--}}
+        {{--//监听加载状态改变--}}
+        {{--document.onreadystatechange = completeLoading;--}}
+        {{--//加载状态为complete时移除loading效果--}}
+        {{--function completeLoading() {--}}
+            {{--if (document.readyState == "complete") {--}}
+                {{--var loadingMask = document.getElementById('loadingDiv');--}}
+                {{--loadingMask.parentNode.removeChild(loadingMask);--}}
 
-            }
-        }
-    </script>
+            {{--}--}}
+        {{--}--}}
+    {{--</script>--}}
 
 @endsection
 
