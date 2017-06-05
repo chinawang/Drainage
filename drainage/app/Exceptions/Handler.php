aaa1786b9809185b4de7a7df49a6591dc2417522
Wangyx <chinawangyx@hotmail.com>
2017-06-05 17:51:19 +0800

    update export code

--- drainage/app/Exceptions/Handler.php
+++ drainage/app/Exceptions/Handler.php
@@ -45,13 +45,20 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($exception instanceof HttpException) {
-            $code = $exception->getStatusCode();
-            if (view()->exists('error.404')) {
-                $message  = $exception->getMessage();
-                return response()->view('error.404', ['message'=>$message], $exception->getStatusCode());
-            }
+//        if ($exception instanceof HttpException) {
+//            $code = $exception->getStatusCode();
+//            if (view()->exists('error.404')) {
+//                $message  = $exception->getMessage();
+//                return response()->view('error.404', ['message'=>$message], $exception->getStatusCode());
+//            }
+//        }
+
+        $code = $exception->getStatusCode();
+        if (view()->exists('error.404')) {
+            $message  = $exception->getMessage();
+            return response()->view('error.404', ['message'=>$message], $exception->getStatusCode());
         }
+
         return parent::render($request, $exception);
 
 //        return parent::render($request, $exception);
