c829feac8288b4936de0b8d963b34f314c9a021d
Wangyx <chinawangyx@hotmail.com>
2017-06-05 17:34:00 +0800

    update export code

--- drainage/resources/views/error/404.blade.php
+++ drainage/resources/views/error/404.blade.php
@@ -8,7 +8,7 @@
                      id="content-weather">
                     <div class="panel-body custom-panel-body">
                         <div style="text-align: center;margin-top: 40px">
-                            <img style="width: 400px" src="/img/error/404.png">
+                            <img style="width: 400px" src="/img/error/sorry.jpg">
                         </div>
                         <div style="text-align: center;margin-top: 60px;color: #75757d;">很抱歉, 系统异常, 请稍后重试。</div>
                         <div style="text-align: center;margin-top: 20px;margin-bottom: 40px"><a href="{{ url('/') }}">返回首页</a></div>
