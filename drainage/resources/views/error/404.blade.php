369d4db5bcf950c58ec8293999ccdb19cf942361
Wangyx <chinawangyx@hotmail.com>
2017-06-05 17:40:17 +0800

    update export code

--- drainage/resources/views/error/404.blade.php
+++ drainage/resources/views/error/404.blade.php
@@ -17,7 +17,7 @@
                         <div style="text-align: center;margin-top: 40px">
                             <img style="width: 100px" src="/img/error/404.png">
                         </div>
-                        <div style="text-align: center;margin-top: 60px;color: #75757d;">很抱歉, 系统异常, 请稍后重试。</div>
+                        <div style="text-align: center;margin-top: 40px;color: #75757d;">很抱歉, 系统异常, 请稍后重试。</div>
                         <div style="text-align: center;margin-top: 20px;margin-bottom: 40px"><a href="{{ url('/') }}">返回首页</a></div>
                     </div>
                 </div>
