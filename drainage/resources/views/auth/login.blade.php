c81e7bdcc2a232aee9a9c1a9377a241f4854a161
Wangyx <chinawangyx@hotmail.com>
2017-06-07 11:37:48 +0800

    back img

--- drainage/resources/views/auth/login.blade.php
+++ drainage/resources/views/auth/login.blade.php
@@ -5,70 +5,68 @@
 @endsection
 
 @section('content')
-    <div style="background: transparent url(/img/back-water.jpeg) no-repeat center center; width:100%; height:100%; z-index:-1">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">登录</div>
-                        <div class="panel-body">
-                            <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                                {{ csrf_field() }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">登录</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
 
-                                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                    <label for="name" class="col-md-4 control-label">账号</label>
+                            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                <label for="name" class="col-md-4 control-label">账号</label>
 
-                                    <div class="col-md-6">
-                                        <input id="name" type="name" class="form-control" name="name"
-                                               value="{{ old('name') }}" required autofocus>
+                                <div class="col-md-6">
+                                    <input id="name" type="name" class="form-control" name="name"
+                                           value="{{ old('name') }}" required autofocus>
 
-                                        @if ($errors->has('name'))
-                                            <span class="help-block">
+                                    @if ($errors->has('name'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                                        @endif
-                                    </div>
+                                    @endif
                                 </div>
+                            </div>
 
-                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                    <label for="password" class="col-md-4 control-label">密码</label>
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password" class="col-md-4 control-label">密码</label>
 
-                                    <div class="col-md-6">
-                                        <input id="password" type="password" class="form-control" name="password"
-                                               required>
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password"
+                                           required>
 
-                                        @if ($errors->has('password'))
-                                            <span class="help-block">
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                        @endif
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox"
+                                                   name="remember" {{ old('remember') ? 'checked' : '' }}> 记住我
+                                        </label>
                                     </div>
                                 </div>
+                            </div>
 
-                                <div class="form-group">
-                                    <div class="col-md-6 col-md-offset-4">
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox"
-                                                       name="remember" {{ old('remember') ? 'checked' : '' }}> 记住我
-                                            </label>
-                                        </div>
-                                    </div>
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        登录
+                                    </button>
+
+                                    {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
+                                    {{--忘记密码?--}}
+                                    {{--</a>--}}
                                 </div>
-
-                                <div class="form-group">
-                                    <div class="col-md-8 col-md-offset-4">
-                                        <button type="submit" class="btn btn-primary">
-                                            登录
-                                        </button>
-
-                                        {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
-                                        {{--忘记密码?--}}
-                                        {{--</a>--}}
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
