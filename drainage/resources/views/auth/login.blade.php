30dcaa0150e6ab9f5cbb60ccb26748490dc5a6e6
Wangyx <chinawangyx@hotmail.com>
2017-06-07 11:19:01 +0800

    update back img

--- drainage/resources/views/auth/login.blade.php
+++ drainage/resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <div style="background-image: url('/img/back-water.jpeg');">
+    <div style="background: transparent url(/img/back-water.jpeg) no-repeat center center;">
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
