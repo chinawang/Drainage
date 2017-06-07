6111b08d15f31b6db16c6da5435a8cc394501d35
Wangyx <chinawangyx@hotmail.com>
2017-06-07 12:07:09 +0800

    back img

--- drainage/resources/views/auth/login.blade.php
+++ drainage/resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
     <div class="container">
         <div class="row" style="margin-top: 100px">
             <div class="col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
+                <div class="panel panel-default custom-panel">
                     <div class="panel-heading">登录</div>
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
