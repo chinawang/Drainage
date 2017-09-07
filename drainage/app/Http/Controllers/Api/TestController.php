1c5ae3d2a4c675d2efc4badbd5b4db788439ac43
Wangyx <chinawangyx@hotmail.com>
2017-09-07 21:10:04 +0800

    api test

--- drainage/app/Http/Controllers/Api/TestController.php
+++ drainage/app/Http/Controllers/Api/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
