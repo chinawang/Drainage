7915ecffb4c0a1d5daca5bd4d05cc7e8bb4341e6
Wangyx <chinawangyx@hotmail.com>
2017-09-25 02:52:11 +0800

    update status report page

--- drainage/resources/views/report/stationStatusMonth.blade.php
+++ drainage/resources/views/report/stationStatusMonth.blade.php
@@ -257,7 +257,7 @@
         var datas4 = [];
 
         $.each(statusRTList.stations, function (i, n) {
-            categories[i] = n["id"];
+            categories[i] = n[0]["name"];
             datas1[i] = n["totalTimeDay1"];
             datas2[i] = n["totalTimeDay2"];
             datas3[i] = n["totalTimeDay3"];
