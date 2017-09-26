174fa123e5d11bd058320a4122e36589f89556a8
Wangyx <chinawangyx@hotmail.com>
2017-09-26 18:30:38 +0800

    update login page

--- drainage/resources/views/auth/login.blade.php
+++ drainage/resources/views/auth/login.blade.php
@@ -10,13 +10,13 @@
 
 @section('content')
     <div class="container">
-        <div class="row" style="margin-top: 100px" >
-            <div class="col-md-4 col-md-offset-4 logo">
-                <img src="/img/header/logo.png" alt="logo">
+        <div class="row" style="margin-top: 20px" >
+            <div class="col-md-12 col-md-offset-0 logo">
+                <img src="/img/header/logo_big.png" alt="logo">
             </div>
         </div>
-        <div class="row" style="margin-top: 10px">
-            <div class="col-md-4 col-md-offset-4">
+        <div class="row">
+            <div class="col-md-12 col-md-offset-0">
                 <span class="brand">{{ config('app.name', 'Laravel') }}</span>
             </div>
         </div>
