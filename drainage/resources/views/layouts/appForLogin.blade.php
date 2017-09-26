c44d82656380985fbddad31e7984ac391f8fbe7e
Wangyx <chinawangyx@hotmail.com>
2017-09-26 18:41:05 +0800

    update login page

--- drainage/resources/views/layouts/appForLogin.blade.php
+++ drainage/resources/views/layouts/appForLogin.blade.php
@@ -37,7 +37,7 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-default navbar-static-top">
+    <nav class="navbar navbar-default navbar-static-top" style="padding-top: 10px">
         <div class="container">
             <div class="navbar-header">
 
