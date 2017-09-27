0abcc1ba8a6cf7a5bf3669ff27abb292775749be
Wangyx <chinawangyx@hotmail.com>
2017-09-27 20:17:34 +0800

    warning report code

--- drainage/resources/views/map/map.blade.php
+++ drainage/resources/views/map/map.blade.php
@@ -71,7 +71,7 @@
         var map = new AMap.Map('map-container', {
             zoom: 12,
             center: [113.658578, 34.746427],
-            mapStyle: 'normal',
+            mapStyle: 'grey',
             resizeEnable: true
         });
 
