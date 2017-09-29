d74507e886d863358dee98662b311ebaa89b8fa8
Wangyx <chinawangyx@hotmail.com>
2017-09-29 19:45:07 +0800

    status report code

--- drainage/app/Http/Controllers/Reporte/StatusReportController.php
+++ drainage/app/Http/Controllers/Reporte/StatusReportController.php
@@ -65,7 +65,7 @@ class StatusReportController extends Controller
 
         $param = $this->getStatusReport($stationID,$startTime,$endTime);
 
-//        return $param;
+        return $param;
 
         //记录Log
         app('App\Http\Logic\Log\LogLogic')->createLog(['name' => Auth::user()->name,'log' => '查看了泵站启动状态统计']);
