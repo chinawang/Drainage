9042b9b9502eafe71976a0845f8ba8f81b6ba652
Wangyx <chinawangyx@hotmail.com>
2017-09-30 23:13:34 +0800

    status report code

--- drainage/app/Http/Controllers/Reporte/StatusReportController.php
+++ drainage/app/Http/Controllers/Reporte/StatusReportController.php
@@ -66,7 +66,7 @@ class StatusReportController extends Controller
         //连前累计
         $beforeTime = date("2017-09-01");
 
-        return $this->getStationRTAll($stationID,$beforeTime,$endTime);
+//        return $this->getStationRTAll($stationID,$beforeTime,$endTime);
 
         $statusReportDay = $this->getStatusReport($stationID,$startTime,$endTime);
 
