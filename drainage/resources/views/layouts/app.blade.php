3bb9085fc3c25555ced882b665c65742272d7bfc
Wangyx <chinawangyx@hotmail.com>
2017-10-01 00:18:58 +0800

    status report code

--- drainage/resources/views/layouts/app.blade.php
+++ drainage/resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
     <link href="{{ asset('css/sweetalert.css') }}" rel="stylesheet">
     <link href="{{ asset('css/common/header.css') }}" rel="stylesheet">
 
-    <link href="{{ asset('css/pace/themes/black/pace-theme-loading-bar.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/pace/themes/yellow/pace-theme-loading-bar.css') }}" rel="stylesheet">
 
 @yield('stylesheet')
 
