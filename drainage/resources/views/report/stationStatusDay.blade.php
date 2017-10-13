ed517911a5d47f3ad876a8877e55f51a934bc210
Wangyx <chinawangyx@hotmail.com>
2017-10-13 23:11:08 +0800

    update permission name to slug

--- drainage/resources/views/report/stationStatusDay.blade.php
+++ drainage/resources/views/report/stationStatusDay.blade.php
@@ -709,66 +709,66 @@
 //        });
 
 
-        var chartAll = new Highcharts.Chart('pumpAllContainer', {
-            chart: {
-                type: 'xrange'
-            },
-            title: {
-                text: '',
-                x: -20
-            },
-            subtitle: {
-                text: '',
-                x: -20
-            },
-            xAxis: {
-                type: 'datetime',
-            },
-            yAxis: {
-                title: {
-                    text: ''
-                },
-                categories: ['1号泵', '2号泵', '3号泵', '4号泵', '5号泵'],
-                reversed: true
-            },
-            tooltip: {
-                valueSuffix: '分钟'
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
-            },
-            series: [{
-                name: '泵组运行时间',
-                borderColor: 'gray',
-                pointWidth: 20,
-                data: [{
-                    x: Date.UTC(2014, 10, 21),
-                    x2: Date.UTC(2014, 11, 2),
-                    y: 0,
-                    partialFill: 0.25
-                }, {
-                    x: Date.UTC(2014, 11, 2),
-                    x2: Date.UTC(2014, 11, 5),
-                    y: 1
-                }, {
-                    x: Date.UTC(2014, 11, 8),
-                    x2: Date.UTC(2014, 11, 9),
-                    y: 2
-                }, {
-                    x: Date.UTC(2014, 11, 9),
-                    x2: Date.UTC(2014, 11, 19),
-                    y: 1
-                }, {
-                    x: Date.UTC(2014, 11, 10),
-                    x2: Date.UTC(2014, 11, 23),
-                    y: 2
-                }],
-            },
-            ]
-        });
+//        var chartAll = new Highcharts.Chart('pumpAllContainer', {
+//            chart: {
+//                type: 'xrange'
+//            },
+//            title: {
+//                text: '',
+//                x: -20
+//            },
+//            subtitle: {
+//                text: '',
+//                x: -20
+//            },
+//            xAxis: {
+//                type: 'datetime',
+//            },
+//            yAxis: {
+//                title: {
+//                    text: ''
+//                },
+//                categories: ['1号泵', '2号泵', '3号泵', '4号泵', '5号泵'],
+//                reversed: true
+//            },
+//            tooltip: {
+//                valueSuffix: '分钟'
+//            },
+//            legend: {
+//                layout: 'vertical',
+//                align: 'right',
+//                verticalAlign: 'middle',
+//                borderWidth: 0
+//            },
+//            series: [{
+//                name: '泵组运行时间',
+//                borderColor: 'gray',
+//                pointWidth: 20,
+//                data: [{
+//                    x: Date.UTC(2014, 10, 21),
+//                    x2: Date.UTC(2014, 11, 2),
+//                    y: 0,
+//                    partialFill: 0.25
+//                }, {
+//                    x: Date.UTC(2014, 11, 2),
+//                    x2: Date.UTC(2014, 11, 5),
+//                    y: 1
+//                }, {
+//                    x: Date.UTC(2014, 11, 8),
+//                    x2: Date.UTC(2014, 11, 9),
+//                    y: 2
+//                }, {
+//                    x: Date.UTC(2014, 11, 9),
+//                    x2: Date.UTC(2014, 11, 19),
+//                    y: 1
+//                }, {
+//                    x: Date.UTC(2014, 11, 10),
+//                    x2: Date.UTC(2014, 11, 23),
+//                    y: 2
+//                }],
+//            },
+//            ]
+//        });
 
         Highcharts.chart('container', {
             chart: {
