e776d2905ca17319b9720a65b672a6de6d05b9b5
Wangyx <chinawangyx@hotmail.com>
2017-11-12 14:52:48 +0800

    update equipment

--- drainage/resources/views/layouts/app.blade.php
+++ drainage/resources/views/layouts/app.blade.php
@@ -206,7 +206,7 @@
         pickerPosition: "bottom-right",
         language: 'zh-CN',//中文，需要引用zh-CN.js包
         startView: 2,//月视图
-        minView: 1//日期时间选择器所能够提供的最精确的时间选择视图
+        minView: 2//日期时间选择器所能够提供的最精确的时间选择视图
     });
 
 </script>
