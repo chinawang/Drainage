a1be10b1a1bea595138b97ac0dccc1882bd62196
Wangyx <chinawangyx@hotmail.com>
2017-12-03 21:33:18 +0800

    update station status line

--- drainage/resources/views/report/stationStatusOverAll.blade.php
+++ drainage/resources/views/report/stationStatusOverAll.blade.php
@@ -161,15 +161,15 @@
                                 <div style="overflow: auto">
                                 @if($selectType == '雨水')
                                     <div class="panel-body custom-panel-body" id="container"
-                                         style="min-width:1240px;height:5400px;margin-left: -10px;">
+                                         style="min-width:1240px;height:5100px;margin-left: -10px;">
                                     </div>
                                 @elseif($selectType == '污水')
                                     <div class="panel-body custom-panel-body" id="container"
-                                         style="min-width:1240px;height:2400px;margin-left: -10px;">
+                                         style="min-width:1240px;height:2000px;margin-left: -10px;">
                                     </div>
                                 @else
                                     <div class="panel-body custom-panel-body " id="container"
-                                         style="min-width:1240px;height:7200px;margin-left: -10px;">
+                                         style="min-width:1240px;height:7000px;margin-left: -10px;">
                                     </div>
                                 @endif
                                 </div>
