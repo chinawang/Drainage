bd84120cf143f671b202987193bab4582d65c3d0
Wangyx <chinawangyx@hotmail.com>
2017-12-03 15:39:15 +0800

    update station status line

--- drainage/resources/views/report/stationStatusOverAll.blade.php
+++ drainage/resources/views/report/stationStatusOverAll.blade.php
@@ -386,8 +386,8 @@
 //                type: 'datetime',
 //                xDateFormat: '%Y-%m-%d %H:%M:%S',
                 useHTML: true,
-                headerFormat: '<h5>运行区间: {point.x} <span>-</span>{point.x2} </h5><br>',
-                pointFormat: '<h5>运行时间：{point.y} 分钟 </h5>',
+                headerFormat: '<h5 style="color:#18BC9C">运行区间: {point.x} <span>-</span>{point.x2} </h5>',
+                pointFormat: '<h5 style="color:#18BC9C">运行时间：{point.y} 分钟 </h5>',
                 xDateFormat: '%H:%M:%S',
 //                dateTimeLabelFormats: {
 //                    day: '%Y-%m-%d',
