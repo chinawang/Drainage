99584b6dfbe842433ff3a9094b114dda63c73656
Wangyx <chinawangyx@hotmail.com>
2017-12-06 12:04:01 +0800

    update station status line

--- drainage/resources/views/failure/addFailure.blade.php
+++ drainage/resources/views/failure/addFailure.blade.php
@@ -38,7 +38,7 @@
                                 <label for="station" class="col-md-4 control-label">所属泵站</label>
 
                                 <div class="col-md-6">
-                                    <select class="form-control" id="select" name="station_id">
+                                    <select class="form-control" id="select" name="station_id" required>
                                         <option value="" selected="selected" style="display: none">选择泵站</option>
                                         @foreach ($stations as $station)
                                             <option value="{{ $station['id'] }}">{{ $station['name'] }}</option>
@@ -56,7 +56,7 @@
                                 <label for="equipment" class="col-md-4 control-label">故障设备</label>
 
                                 <div class="col-md-6">
-                                    <select class="form-control" id="select" name="equipment_id">
+                                    <select class="form-control" id="select" name="equipment_id" >
                                         <option value="" selected="selected" style="display: none">选择设备</option>
                                         @foreach ($equipments as $equipment)
                                             <option value="{{ $equipment['id'] }}">{{ $equipment['name'] }}</option>
