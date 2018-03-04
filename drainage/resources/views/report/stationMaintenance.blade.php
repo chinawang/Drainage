57e5cf225e31e7ac01b170cd004182693df86a1d
Wangyx <chinawangyx@hotmail.com>
2018-03-04 16:06:07 +0800

    update failure and maintence repairtor

--- drainage/resources/views/report/stationMaintenance.blade.php
+++ drainage/resources/views/report/stationMaintenance.blade.php
@@ -47,7 +47,7 @@
                                 {{ csrf_field() }}
 
                                 <div class="row">
-                                    <div class="col-md-2">
+                                    <div class="col-md-3">
                                         <div class="form-group">
                                             <label for="station" class="col-md-4 control-label">选择泵站:</label>
 
@@ -57,26 +57,6 @@
                                                     @foreach ($stations as $station)
                                                         <option value="{{ $station['id'] }}" {{$station['id'] == $stationSelect['id'] ? 'selected=selected' :''}}>{{ $station['name'] }}</option>
                                                     @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <div class="form-group">
-                                            <label for="station" class="col-md-4 control-label">维修进度:</label>
-
-                                            <div class="col-md-8">
-                                                <select class="form-control" id="select" name="repairProcess">
-                                                    <option value="全部" selected="selected">全部</option>
-                                                    <option value="0" {{$selectProcess == '0' ? 'selected=selected' :''}}>
-                                                        报修
-                                                    </option>
-                                                    <option value="1" {{$selectProcess == '1' ? 'selected=selected' :''}}>
-                                                        维修中
-                                                    </option>
-                                                    <option value="2" {{$selectProcess == '2' ? 'selected=selected' :''}}>
-                                                        维修完成
-                                                    </option>
                                                 </select>
                                             </div>
                                         </div>
@@ -97,9 +77,9 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-2">
+                                    <div class="col-md-3">
                                         <div class="form-group">
-                                            <div class="col-md-6 col-md-offset-4">
+                                            <div class="col-md-4 col-md-offset-1">
                                                 <button type="submit" class="btn btn-primary btn-custom">
                                                     <span class="glyphicon glyphicon-search"></span>
                                                     查询
@@ -111,6 +91,28 @@
                                                     <span class="glyphicon glyphicon-export"></span>
                                                     导出报表
                                                 </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row" style="margin-left: -60px">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="station" class="col-md-4 control-label">维修进度:</label>
+
+                                            <div class="col-md-8">
+                                                <select class="form-control" id="select" name="repairProcess">
+                                                    <option value="全部" selected="selected">全部</option>
+                                                    <option value="0" {{$selectProcess == '0' ? 'selected=selected' :''}}>
+                                                        报修
+                                                    </option>
+                                                    <option value="1" {{$selectProcess == '1' ? 'selected=selected' :''}}>
+                                                        维修中
+                                                    </option>
+                                                    <option value="2" {{$selectProcess == '2' ? 'selected=selected' :''}}>
+                                                        维修完成
+                                                    </option>
+                                                </select>
                                             </div>
                                         </div>
                                     </div>
