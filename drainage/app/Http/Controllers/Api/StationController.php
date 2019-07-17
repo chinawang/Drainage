59d838e7cf8076a75ede075c15932df781185d78
Wangyx <chinawangyx@hotmail.com>
2019-07-17 21:50:15 +0800

    add api

--- drainage/app/Http/Controllers/Api/StationController.php
+++ drainage/app/Http/Controllers/Api/StationController.php
@@ -956,11 +956,11 @@ class StationController extends Controller
 
     public function getStationRTs($stationNum)
     {
-//        $nowTime = date("Y-m-d H:i:s", strtotime("-2 Minute"));
-        $nowTime = date("2017-01-01 00:00:00", strtotime("-2 Minute"));
+        $nowTime = date("Y-m-d H:i:s", strtotime("-2 Minute"));
 
         $stationTable = "stationRT_" . $stationNum;
-        $stationRTs = DB::select('select * from ' . $stationTable . ' WHERE Time > ? order by Time desc limit 1', [$nowTime]);
+//        $stationRTs = DB::select('select * from ' . $stationTable . ' WHERE Time > ? order by Time desc limit 1', [$nowTime]);
+        $stationRTs = DB::select('select * from ' . $stationTable . ' order by Time desc limit 1');
 //        $stationRTs = DB::select('SELECT * FROM  '.$stationTable.' WHERE Time = (select max(Time) AS maxTime from  '.$stationTable.' WHERE Time > ?)',[$nowTime]);
         return $stationRTs;
     }
